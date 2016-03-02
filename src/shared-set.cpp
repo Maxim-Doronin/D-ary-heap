e154695846fa08c0fdf50e63aeaf04ea1813e0a5
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2016-03-02 10:34:39 +0300

    SharedSet implemented

---
+++ src/shared-set.cpp
@@ -0,0 +1,61 @@
+#include "shared-set.h"
+
+SharedSet::SharedSet(int n)
+{
+	parent = new int [n];
+	for (int i = 0; i < n; i++)
+		parent[i] = -1;
+	height = new int [n];
+	this->n = n;
+}
+
+SharedSet::~SharedSet()
+{
+	delete [] parent;
+	delete [] height;
+}
+
+bool SharedSet::validIndex(int i)
+{
+	if ((i < 0) || (i >= n))
+		return false;
+	else
+		return true;
+}
+
+void SharedSet::singleton(int i)
+{
+	if (!validIndex(i))
+		throw "SharedSet: Invalid Index!";
+	if (parent[i] != -1)
+		return;
+	parent[i] = i;
+	height[i] = 0;
+}
+
+void SharedSet::association(int i, int j)
+{
+	if ((!validIndex(i)) || (!validIndex(j)))
+		throw "SharedSet: Invalid Indexes!";
+	if ((parent[i] == -1)||(parent[j] == -1))
+		throw "SharedSet: Empty set!";
+	if (height[i] > height[j])
+		parent[j] = i;
+	else if (height[i] < height[j])
+		parent[i] = j;
+	else {
+		parent[j] = i;
+		height[i]++;
+	}
+}
+
+int SharedSet::defineSet(int i)
+{
+	if (!validIndex(i))
+		throw "SharedSet: Invalid Index!";
+	if (parent[i] == -1)
+		return -1;
+	while (parent[i] != i)
+		i = parent[i];
+	return i;
+}
